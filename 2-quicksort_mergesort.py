54effd36c95328d70e557a5d21b3fdb2372ee7ed
Ted <58144503+BUSH222@users.noreply.github.com>
2025-03-26 23:24:35 +0300

    mergesort

--- 2-quicksort_mergesort.py
+++ 2-quicksort_mergesort.py
@@ -23,8 +23,51 @@ def rearrange(arr, low, high):
     return i + 1
 
 
+# N = int(input())
+# arr = list(map(int, input().split()))
+# quicksort(arr, 0, N - 1)
+# print(' '.join(map(str, arr)))
+
+
+# n2
+# TEST - merge_sort.zip
+
+
+def mergesort(arr, thing, left, right):
+    if left >= right:
+        return
+
+    mid = (left + right) // 2
+
+    mergesort(arr, thing, left, mid)
+    mergesort(arr, thing, mid + 1, right)
+
+    merge(arr, thing, left, mid, right)
+
+
+def merge(arr, thing, left, mid, right):
+    for i in range(left, right + 1):
+        thing[i] = arr[i]
+
+    i, j, k = left, mid + 1, left
+
+    while i <= mid and j <= right:
+        if thing[i] <= thing[j]:
+            arr[k] = thing[i]
+            i += 1
+        else:
+            arr[k] = thing[j]
+            j += 1
+        k += 1
+
+    while i <= mid:
+        arr[k] = thing[i]
+        i += 1
+        k += 1
+
+
 N = int(input())
 arr = list(map(int, input().split()))
-quicksort(arr, 0, N - 1)
+thing = [0] * N
+mergesort(arr, thing, 0, N - 1)
 print(' '.join(map(str, arr)))
-
