420f03c5f0d07f0ecd347cf85d3ab63872f510e0
Ted <58144503+BUSH222@users.noreply.github.com>
2025-03-31 11:38:22 +0300

    contest 2 done

--- 2-quicksort_mergesort.py
+++ 2-quicksort_mergesort.py
@@ -66,8 +66,40 @@ def merge(arr, thing, left, mid, right):
         k += 1
 
 
+# N = int(input())
+# arr = list(map(int, input().split()))
+# thing = [0] * N
+# mergesort(arr, thing, 0, N - 1)
+# print(' '.join(map(str, arr)))
+
+
+# n3
+# TEST - merge_sort.zip
+# I will implement a heapsort
+
+
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+    if left < n and arr[i] < arr[left]:
+        largest = left
+    if right < n and arr[largest] < arr[right]:
+        largest = right
+    if largest != i:
+        (arr[i], arr[largest]) = (arr[largest], arr[i])  # swap
+        heapify(arr, n, largest)
+
+
+def heap_sort(arr, n):
+    for i in range(n // 2, -1, -1):
+        heapify(arr, n, i)
+    for i in range(n - 1, 0, -1):
+        (arr[i], arr[0]) = (arr[0], arr[i])
+        heapify(arr, i, 0)
+
+
 N = int(input())
 arr = list(map(int, input().split()))
-thing = [0] * N
-mergesort(arr, thing, 0, N - 1)
+heap_sort(arr, N)
 print(' '.join(map(str, arr)))
